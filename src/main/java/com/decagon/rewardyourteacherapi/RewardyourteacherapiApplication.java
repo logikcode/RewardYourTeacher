a25ea5440f505818147f84bd33904cf50af2ce26
EMMANUEL MACAULAY <emmanuelmcl86@gmail.com>
2022-09-10 12:36:49 +0100

    first commit

---
+++ src/main/java/com/decagon/rewardyourteacherapi/RewardyourteacherapiApplication.java
@@ -0,0 +1,13 @@
+package com.decagon.rewardyourteacherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardyourteacherapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardyourteacherapiApplication.class, args);
+	}
+
+}
