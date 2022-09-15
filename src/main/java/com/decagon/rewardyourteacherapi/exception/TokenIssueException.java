23e3409dbb73f88839d6c34697429ee5e685c3e1
Emmanuel Oladipupo <manueltech401@gmail.com>
2022-09-15 20:04:18 +0100

    removed the @NoArgsConstructor annotation

--- src/main/java/com/decagon/rewardyourteacherapi/exception/TokenIssueException.java
+++ src/main/java/com/decagon/rewardyourteacherapi/exception/TokenIssueException.java
@@ -2,11 +2,9 @@ package com.decagon.rewardyourteacherapi.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class TokenIssueException extends RuntimeException {
 
