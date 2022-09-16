1d365b0a32cfd5e3136f130a1b966a11307c8bbf
codeKonnects <topedaves@gmail.co>
2022-09-16 10:08:47 +0100

    Implemented save notification service

---
+++ src/main/java/com/decagon/rewardyourteacherapi/exception/ErrorMessage.java
@@ -0,0 +1,16 @@
+package com.decagon.rewardyourteacherapi.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+    private Date timestamp;
+    private String message;
+    private String details;
+}
