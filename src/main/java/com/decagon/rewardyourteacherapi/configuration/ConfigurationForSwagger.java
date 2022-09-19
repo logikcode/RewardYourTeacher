a2faebaeff61ddf48444d56732c6a8661b19194a
VALOPURUM <100851243+VALOPURUM@users.noreply.github.com>
2022-09-19 10:28:27 +0100

    My configuration commit

--- src/main/java/com/decagon/rewardyourteacherapi/configuration/ConfigurationForSwagger.java
+++ src/main/java/com/decagon/rewardyourteacherapi/configuration/ConfigurationForSwagger.java
@@ -1,11 +1,9 @@
-package com.decagon.rewardyourteacherapi.config;
+package com.decagon.rewardyourteacherapi.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
