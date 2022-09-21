8076664af6ff385c0906729304cc644ffafd594e
TG <trygod.nwakwasi@decagon.dev>
2022-09-21 19:03:55 +0100

    added a Setter annotation

--- src/main/java/com/decagon/rewardyourteacherapi/dto/TeacherDto.java
+++ src/main/java/com/decagon/rewardyourteacherapi/dto/TeacherDto.java
@@ -4,12 +4,14 @@ import com.decagon.rewardyourteacherapi.enums.SchoolType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class TeacherDto {
 
     private String name;
