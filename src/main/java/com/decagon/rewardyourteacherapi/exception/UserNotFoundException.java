e93be981f6ea19d215d10682b5411a54f1ca2b68
TG <trygod.nwakwasi@decagon.dev>
2022-09-21 19:07:38 +0100

    AllArgsConstructor was removed

--- src/main/java/com/decagon/rewardyourteacherapi/exception/UserNotFoundException.java
+++ src/main/java/com/decagon/rewardyourteacherapi/exception/UserNotFoundException.java
@@ -1,10 +1,12 @@
 package com.decagon.rewardyourteacherapi.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException{
     private String user;
