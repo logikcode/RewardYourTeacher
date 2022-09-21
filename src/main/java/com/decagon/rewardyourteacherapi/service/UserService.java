d11b217139b17b0046e43a3cbfa76c1040b22600
TG <trygod.nwakwasi@decagon.dev>
2022-09-21 19:08:53 +0100

    added the methods for viewing and searching for a teacher

--- src/main/java/com/decagon/rewardyourteacherapi/service/UserService.java
+++ src/main/java/com/decagon/rewardyourteacherapi/service/UserService.java
@@ -4,9 +4,15 @@ import com.decagon.rewardyourteacherapi.dto.StudentDto;
 import com.decagon.rewardyourteacherapi.dto.TeacherDto;
 import com.decagon.rewardyourteacherapi.response.ResponseAPI;
 
+import java.util.List;
+
 public interface UserService {
 
     ResponseAPI<TeacherDto> TeacherSignUp(TeacherDto teacherDto);
 
     ResponseAPI<StudentDto> StudentSignUp(StudentDto studentDto);
+
+    ResponseAPI<TeacherDto> viewTeacher(long id);
+
+    ResponseAPI<List<TeacherDto>> searchForTeacher(String name);
 }
