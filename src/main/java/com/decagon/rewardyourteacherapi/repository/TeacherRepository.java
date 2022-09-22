cffd29f1b24a751b516fc704eb81f3f7fa08f43a
Emmanuel Oladipupo <manueltech401@gmail.com>
2022-09-22 19:53:37 +0100

    added method definition for querying teachers

--- src/main/java/com/decagon/rewardyourteacherapi/repository/TeacherRepository.java
+++ src/main/java/com/decagon/rewardyourteacherapi/repository/TeacherRepository.java
@@ -1,11 +1,14 @@
 package com.decagon.rewardyourteacherapi.repository;
 
 import com.decagon.rewardyourteacherapi.entity.Teacher;
-import com.decagon.rewardyourteacherapi.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 
+    List<Teacher> findAllBySchool(String school, Pageable pageable);
 }
