06a0853490191ffd4158b260f6f7ea9e1bed01ed
Emmanuel Oladipupo <manueltech401@gmail.com>
2022-09-22 19:56:16 +0100

    added method for querying teachers to the interface

--- src/main/java/com/decagon/rewardyourteacherapi/service/UserService.java
+++ src/main/java/com/decagon/rewardyourteacherapi/service/UserService.java
@@ -2,9 +2,11 @@ package com.decagon.rewardyourteacherapi.service;
 
 import com.decagon.rewardyourteacherapi.dto.StudentDto;
 import com.decagon.rewardyourteacherapi.dto.TeacherDto;
+import com.decagon.rewardyourteacherapi.entity.Teacher;
 import com.decagon.rewardyourteacherapi.response.ResponseAPI;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Map;
 
 public interface UserService {
@@ -14,4 +16,5 @@ public interface UserService {
     ResponseAPI<StudentDto> StudentSignUp(StudentDto studentDto);
 
     ResponseAPI<Map<String, Object>> retrieveTeacher(Pageable pageable);
+   ResponseAPI<List<Teacher>> retrieveAllTeachersInSch(String schoolName);
 }
