71f609af081bdc0b131489c19e3c219c7ace3078
Emmanuel Oladipupo <manueltech401@gmail.com>
2022-09-22 21:35:46 +0100

    merge conflict

--- src/main/java/com/decagon/rewardyourteacherapi/dto/SchoolDto.java
+++ src/main/java/com/decagon/rewardyourteacherapi/dto/SchoolDto.java
@@ -1,23 +1,14 @@
 package com.decagon.rewardyourteacherapi.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @Data
-
-public class SchoolDto {
-
-    private String schoolName;
-    private String schoolType;
-    private String schoolAddress;
-    private String schoolCity;
-    private String schoolState;
-
-
-//     "schoolName" : "British International School Lagos",
-//             "schoolType" : "secondary",
-//             "schoolAddress" : "1, Landbridge Avenue, Oniru Private Estate, Victoria Island, Lagos Nigeria",
-//             "schoolCity" : "ikeja",
-//             "schoolState" : "Lagos"
+@AllArgsConstructor
+public class SchoolDTO {
+    private String name;
+    private String type;
+    private String address;
+    private String city;
+    private String state;
 }
--- src/main/java/com/decagon/rewardyourteacherapi/service/UserService.java
+++ src/main/java/com/decagon/rewardyourteacherapi/service/UserService.java
@@ -2,16 +2,12 @@ package com.decagon.rewardyourteacherapi.service;
 
 import com.decagon.rewardyourteacherapi.dto.StudentDto;
 import com.decagon.rewardyourteacherapi.dto.TeacherDto;
-import com.decagon.rewardyourteacherapi.entity.User;
 import com.decagon.rewardyourteacherapi.response.ResponseAPI;
-import com.decagon.rewardyourteacherapi.security.services.CustomUserDetailsService;
 import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
-import java.util.Map;
-import java.util.Optional;
-
 import java.util.List;
+import java.util.Map;
 
 public interface UserService {
 
