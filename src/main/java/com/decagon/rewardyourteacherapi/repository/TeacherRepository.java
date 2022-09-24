82f0c165b472f0c65d0fd52288e987fe40a277bd
Emmanuel Oladipupo <manueltech401@gmail.com>
2022-09-24 15:24:02 +0100

    Added a redefined query method for teacher query

--- src/main/java/com/decagon/rewardyourteacherapi/repository/TeacherRepository.java
+++ src/main/java/com/decagon/rewardyourteacherapi/repository/TeacherRepository.java
@@ -1,6 +1,7 @@
 package com.decagon.rewardyourteacherapi.repository;
 
 import com.decagon.rewardyourteacherapi.entity.Teacher;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,8 @@ import java.util.Optional;
 public interface TeacherRepository extends JpaRepository<Teacher , Long> {
     List<Teacher> findAllByName(String name);
 
-    List<Teacher> findAllBySchool(String school, Pageable pageable);
+   // List<Teacher> findAllBySchool(String school, Pageable pageable);
+    Page<Teacher> findAllBySchoolIgnoreCase(String schoolName, Pageable pageable);
 
     Optional<Teacher> findTeacherByEmail(String email);
 }
